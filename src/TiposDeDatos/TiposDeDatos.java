84a46d33ce213d86c89cf8954674a315df253c80
Hector <hectorpose@gmail.com>
2022-05-30 12:16:23 +0000

    initial commit

---
+++ src/TiposDeDatos/TiposDeDatos.java
@@ -0,0 +1,33 @@
+package TiposDeDatos;
+
+import java.lang.reflect.Array;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        byte variableByte = 1;
+        short variableShort = 2;
+        int variableInt = 3;
+        long variableLong = 4;
+        float variableFloat = 5.0F;
+        double variableDouble = 6.0;
+        boolean variableBoolean = true;
+        char variableChar = 'A';
+        String variableString = "Hola";
+
+
+
+        System.out.println("Byte: " + variableByte);
+        System.out.println("Short: " + variableShort);
+        System.out.println("Int: " + variableInt);
+        System.out.println("Long: " + variableLong);
+        System.out.println("Float: " + variableFloat);
+        System.out.println("Double: " + variableDouble);
+        System.out.println("Boolean: " + variableBoolean);
+        System.out.println("Char: " + variableChar);
+        System.out.println("String: " + variableString);
+    }
+
+
+}
